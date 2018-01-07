d10429ad999b65c049c135766340ede493541907
Bardo91 <19bardo91@gmail.com>
2018-01-07 13:53:47 +0100

    renamed files according to new names in repo

--- arm_controller/src/visualization/ArmVis.cpp
+++ arm_controller/src/visualization/ArmVis.cpp
@@ -14,15 +14,15 @@
 
 bool ArmVis::init(pcl::visualization::PCLVisualizer * _viewer, std::string _folderFiles) {
 	mViewer = _viewer;
-	if (pcl::io::loadPolygonFileSTL(_folderFiles + "/bar1.stl", mBarMesh1) == 0) {
+	if (pcl::io::loadPolygonFileSTL(_folderFiles + "/arm_0_top.stl", mBarMesh1) == 0) {
 		PCL_ERROR("Failed to load STL file\n");
 		return false;
 	}
-	if (pcl::io::loadPolygonFileSTL(_folderFiles + "/bar2.stl", mBarMesh2) == 0) {
+	if (pcl::io::loadPolygonFileSTL(_folderFiles + "/arm_1.stl", mBarMesh2) == 0) {
 		PCL_ERROR("Failed to load STL file\n");
 		return false;
 	}
-	if (pcl::io::loadPolygonFileSTL(_folderFiles + "/bar3.stl", mBarMesh3) == 0) {
+	if (pcl::io::loadPolygonFileSTL(_folderFiles + "/arm_2.stl", mBarMesh3) == 0) {
 		PCL_ERROR("Failed to load STL file\n");
 		return false;
 	}
@@ -38,8 +38,11 @@ void ArmVis::draw(Eigen::Matrix4f  _t1, Eigen::Matrix4f  _t2, Eigen::Matrix4f _t
 	pcl::fromPCLPointCloud2(mBarMesh3.cloud, pctbar3);
 
     pcl::transformPointCloud(pctbar1, pctbar1, _t1);
+	for(auto &p: pctbar1) { p.x /=1000; p.y /=1000;  p.z /=1000;}
 	pcl::transformPointCloud(pctbar2, pctbar2, _t2);
+	for(auto &p: pctbar2) { p.x /=1000; p.y /=1000;  p.z /=1000;}
 	pcl::transformPointCloud(pctbar3, pctbar3, _t3);
+	for(auto &p: pctbar3) { p.x /=1000; p.y /=1000;  p.z /=1000;}
 
     mViewer->addPolygonMesh<pcl::PointXYZ>(pctbar1.makeShared(), mBarMesh1.polygons, "arm_bar1", 0);
     mViewer->addPolygonMesh<pcl::PointXYZ>(pctbar2.makeShared(), mBarMesh2.polygons, "arm_bar2", 0);
