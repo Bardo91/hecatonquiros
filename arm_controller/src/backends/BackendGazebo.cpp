b2cc22a883bfd6b6e76da6ac2d9a5240ccb0976a
Unknown <luismarzor@gmail.com>
2018-02-06 10:48:03 +0000

    Backend Gazebo

--- arm_controller/src/backends/BackendGazebo.cpp
+++ arm_controller/src/backends/BackendGazebo.cpp
@@ -20,27 +20,81 @@
 //---------------------------------------------------------------------------------------------------------------------
 
 #include <arm_controller/backends/BackendGazebo.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Header.h"
+#include <sensor_msgs/JointState.h>
+#include <math.h>
+#include <sstream>
+#include <ros/subscribe_options.h>
+#include <iostream>
+#include <string>
+
 
 namespace hecatonquiros{
+//---------------------------------------------------------------------------------------------------------------------
+    bool BackendGazebo::init(const Config &_config){
+	
+	int cnt=1;
+ 	int joint;
+  	float pos_rad_0,pos_rad_1,pos_rad_2;
+  	
+	
+	ros::NodeHandle n;
+  	mtopic=_config.topic;
+ 	joint_pub = n.advertise<sensor_msgs::JointState>(mtopic, 1000);
+	return true;
+    }
 
     //---------------------------------------------------------------------------------------------------------------------
     bool BackendGazebo::pose(const Eigen::Matrix4f &_pose, bool _blocking){
+	return false;
 
     }
 
     //---------------------------------------------------------------------------------------------------------------------
     bool BackendGazebo::joints(const std::vector<float> &_joints, bool _blocking){
+	//while (ros::ok())
+ 	 //{
+    	
+    	  	int cnt=0;
+	 	sensor_msgs::JointState j_msg; 
+		//std_msgs::String msg;
+		//std::stringstream ss;
+		//ss << "Introduce vector of radians: " ;
+		//msg.data = ss.str();
+		//ROS_INFO("[Talker]:%s ", msg.data.c_str());
+		//std::cin>>pos_rad_0>>pos_rad_1>>pos_rad_2;
+	
+	
+
+			j_msg.header.seq=cnt;
+			j_msg.header.frame_id="0";
+			j_msg.position.push_back (_joints[0]);
+			j_msg.position.push_back (_joints[1]);
+			j_msg.position.push_back (_joints[2]);   
+			j_msg.name.push_back ("arm_0_bottom_joint");
+			j_msg.name.push_back ("arm_1_joint");
+			j_msg.name.push_back ("arm_2_joint");
+			j_msg.header.stamp = ros::Time::now();		
+			ROS_INFO("[Talker]:Publishing");
+	
+
+	
+	    joint_pub.publish(j_msg);   
+	    cnt++;
+	    return true;
+	  //}
+		
 
     }
 
     //---------------------------------------------------------------------------------------------------------------------
     bool BackendGazebo::claw(const int _action){
+	return false;
 
     }
 
-    //---------------------------------------------------------------------------------------------------------------------
-    bool BackendGazebo::init(const Config &_config){
-
-    }
+    
 
 }
