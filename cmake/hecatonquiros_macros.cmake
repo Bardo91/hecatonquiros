fb773becfadc0a81f5622653ceba695747544dc1
Bardo91 <19bardo91@gmail.com>
2018-08-16 09:43:58 +0200

    New code structure catkin style

---
+++ cmake/hecatonquiros_macros.cmake
@@ -0,0 +1,10 @@
+macro(compile_tool tool_name)
+
+	add_executable(${tool_name} ${TOOL_SRC_${tool_name}})
+
+	target_include_directories(${tool_name} PUBLIC ${catkin_INCLUDE_DIRS})
+	target_link_libraries(${tool_name} ${catkin_LIBRARIES} hecatonquiros)
+
+	add_dependencies(${tool_name} ${catkin_EXPORTED_TARGETS} hecatonquiros_generate_messages_cpp)
+
+endmacro()
