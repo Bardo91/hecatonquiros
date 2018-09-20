26b2464f9309b505973810c7a5a9a704e6496ce0
Bardo91 <19bardo91@gmail.com>
2018-09-20 14:56:28 +0200

    finished velocity test

--- tools/velocity_test/src/main.cpp
+++ tools/velocity_test/src/main.cpp
@@ -83,117 +83,191 @@ int main(int _argc, char** _argv){
     std::vector<float> offsetJoints3 = {-0.05, 0.087266463, 0.087266463, 0.0, 0.08726646259, 0.0, -0.05, 0.13089969389, 0.08726646259, -0.08726646259, 0.0, 0.0};
 
     servoDriver =  new SCServo(serialPort);
-    if(servoDriver->isConnected()){
-
-        // ARM 1
-        auto t0 = std::chrono::high_resolution_clock::now();
-        std::vector<float> joints1 = {0.0, 0.0, 0.0, 0.0};
-        unsigned char idn1 = joints1.size();
-        unsigned char id1[idn1];
-        unsigned short pos1[idn1], tim1[idn1], spee1[idn1];
-        for(unsigned i = 0; i < joints1.size(); i++){
-            id1[i] = 10 + i + 1;
-            pos1[i] = mapAngleToVal(minMaxValues1[i].first, minMaxValues1[i].second, joints1[i] + offsetJoints1[i]);
-            tim1[i] = 500;
-            spee1[i] = 0;
-        }
-        guard.lock(); 
-        servoDriver->SyncWritePos(id1, idn1, pos1, tim1, spee1);
-        guard.unlock();
-        auto t1 = std::chrono::high_resolution_clock::now();
-		float duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0).count();
-        std::cout << "TIME SEND JOINTS ARM1: " << duration1 << std::endl;
- 
-        // ARM 2
-        auto t2 = std::chrono::high_resolution_clock::now();
-        std::vector<float> joints2 = {0.0, 0.0, 0.0, 0.0};
-        unsigned char idn2 = joints2.size();
-        unsigned char id2[idn2];
-        unsigned short pos2[idn2], tim2[idn2], spee2[idn2];
-        for(unsigned j = 0; j < joints1.size(); j++){
-            id2[j] = 20 + j + 1;
-            pos2[j] = mapAngleToVal(minMaxValues2[j].first, minMaxValues2[j].second, joints2[j] + offsetJoints2[j]);
-            tim2[j] = 500;
-            spee2[j] = 0;
-        }
-        guard.lock(); 
-        servoDriver->SyncWritePos(id2, idn2, pos2, tim2, spee2);
-        guard.unlock();
-        auto t3 = std::chrono::high_resolution_clock::now();
-		float duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(t3-t2).count();
-        std::cout << "TIME SEND JOINTS ARM2: " << duration2 << std::endl;
-        
-        // BOTH ARMS
-        auto t4 = std::chrono::high_resolution_clock::now();
-        std::vector<float> joints3 = {0.0, 0.0, 0.0, 0.0};
-        std::vector<float> joints4 = {0.0, 0.0, 0.0, 0.0};
-        unsigned char idn3 = joints3.size() + joints4.size();
-        unsigned char id3[idn3];
-        unsigned short pos3[idn3], tim3[idn3], spee3[idn3];
-        for(unsigned k = 0; k < idn3; k++){
-            if(k < joints3.size()){
-                id3[k] = 10 + k + 1;
-                pos3[k] = mapAngleToVal(minMaxValues3[k].first, minMaxValues3[k].second, joints3[k] + offsetJoints3[k]);
-                tim3[k] = 500;
-                spee3[k] = 0;
-            }else{
-                id3[k] = 20 + k + 1;
-                pos3[k] = mapAngleToVal(minMaxValues3[k].first, minMaxValues3[k].second, joints4[k] + offsetJoints3[k]);
-                tim3[k] = 500;
-                spee3[k] = 0;
-            }
-            
-        }
-        guard.lock(); 
-        servoDriver->SyncWritePos(id3, idn3, pos3, tim3, spee3);
-        guard.unlock();
-        auto t5 = std::chrono::high_resolution_clock::now();
-		float duration3 = std::chrono::duration_cast<std::chrono::milliseconds>(t5-t4).count();
-        std::cout << "TIME SEND JOINTS ARM1 & ARM2: " << duration3 << std::endl;
-
-
-        //////////////////////////////////////////////////
-        // READ ARM1
-        auto t6 = std::chrono::high_resolution_clock::now();
-        std::vector<float> joints_read1;
-        for(unsigned l = 0; l < 4; l++){
-            guard.lock();
-            int val = servoDriver->ReadPos(10 + l + 1);
-            guard.unlock();
-            if(val < 0 || val > 1024){
-                joints_read1.push_back(-1);
-            }else{
-                joints_read1.push_back( mapValToAngle( minMaxValues1[l].first, 
-                                                minMaxValues1[l].second,
-                                                val) - offsetJoints1[l]);
-            }
-        }
-        auto t7 = std::chrono::high_resolution_clock::now();
-		float duration4 = std::chrono::duration_cast<std::chrono::milliseconds>(t7-t6).count();
-        std::cout << "TIME READ JOINTS ARM1: " << duration4 << std::endl;
-
-        // READ ARM2
-        auto t8 = std::chrono::high_resolution_clock::now();
-        std::vector<float> joints_read2;
-        for(unsigned m = 0; m < 4; m++){
-            guard.lock();
-            int val = servoDriver->ReadPos(10 + m + 1);
-            guard.unlock();
-            if(val < 0 || val > 1024){
-                joints_read2.push_back(-1);
-            }else{
-                joints_read2.push_back( mapValToAngle( minMaxValues2[m].first, 
-                                                minMaxValues2[m].second,
-                                                val) - offsetJoints2[m]);
-            }
-        }
-        auto t9 = std::chrono::high_resolution_clock::now();
-		float duration5 = std::chrono::duration_cast<std::chrono::milliseconds>(t9-t8).count();
-        std::cout << "TIME READ JOINTS ARM2: " << duration5 << std::endl;
-
-
-
+    if(!servoDriver->isConnected()){
+        return -1;
     }
+
+    // ARM 1
+    auto t0 = std::chrono::high_resolution_clock::now();
+    std::vector<float> joints1 = {0.0*M_PI/180.0, 0.0*M_PI/180.0, 90.0*M_PI/180.0, 0.0*M_PI/180.0};
+    unsigned char idn1 = joints1.size();
+    unsigned char id1[idn1];
+    unsigned short pos1[idn1], tim1[idn1], spee1[idn1];
+    for(unsigned i = 0; i < joints1.size(); i++){
+        id1[i] = 10 + i + 1;
+        pos1[i] = mapAngleToVal(minMaxValues1[i].first, minMaxValues1[i].second, joints1[i] + offsetJoints1[i]);
+        tim1[i] = 500;
+        spee1[i] = 0;
+    }
+    guard.lock(); 
+    servoDriver->SyncWritePos(id1, idn1, pos1, tim1, spee1);
+    guard.unlock();
+    auto t1 = std::chrono::high_resolution_clock::now();
+    float duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0).count();
+    std::cout << "Time send joints ARM1: " << std::to_string(duration1) << std::endl;
+    std::cout << "Position sended ARM1: " << std::endl;
+    for(unsigned i = 0; i < idn1; i++){
+        std::cout << "  " << std::to_string(id1[i]) << "  " << pos1[i] << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    // ARM 2
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::vector<float> joints2 = {0.0*M_PI/180.0, 0.0*M_PI/180.0, 90.0*M_PI/180.0, 0.0*M_PI/180.0};
+    unsigned char idn2 = joints2.size();
+    unsigned char id2[idn2];
+    unsigned short pos2[idn2], tim2[idn2], spee2[idn2];
+    for(unsigned j = 0; j < joints1.size(); j++){
+        id2[j] = 20 + j + 1;
+        pos2[j] = mapAngleToVal(minMaxValues2[j].first, minMaxValues2[j].second, joints2[j] + offsetJoints2[j]);
+        tim2[j] = 500;
+        spee2[j] = 0;
+    }
+    guard.lock(); 
+    servoDriver->SyncWritePos(id2, idn2, pos2, tim2, spee2);
+    guard.unlock();
+    auto t3 = std::chrono::high_resolution_clock::now();
+    float duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(t3-t2).count();
+    std::cout << "Time send joints ARM2: " << std::to_string(duration2) << std::endl;
+    std::cout << "Position sended ARM2: " << std::endl;
+    for(unsigned j = 0; j < idn2; j++){
+        std::cout << "  " << std::to_string(id2[j]) << "  " << pos2[j] << std::endl;
+    }
+    std::cout << std::endl;
+    
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    // BOTH ARMS
+    auto t4 = std::chrono::high_resolution_clock::now();
+    std::vector<float> joints3 = {0.0*M_PI/180.0, 0.0*M_PI/180.0, 45.0*M_PI/180.0, 0.0*M_PI/180.0, 0.0*M_PI/180.0, 0.0*M_PI/180.0, 45.0*M_PI/180.0, 0.0*M_PI/180.0};
+    unsigned char idn3 = joints3.size();
+    unsigned char id3[idn3];
+    unsigned short pos3[idn3], tim3[idn3], spee3[idn3];
+    int cont1 = 0, cont2 = 0;
+    for(unsigned k = 0; k < joints3.size(); k++){
+        if(k < 4){
+            id3[k] = 10 + cont1 + 1;
+            pos3[k] = mapAngleToVal(minMaxValues3[k].first, minMaxValues3[k].second, joints3[k] + offsetJoints3[k]);
+            tim3[k] = 500;
+            spee3[k] = 0;
+            cont1++;
+        }else{
+            id3[k] = 20 + cont2 + 1;
+            pos3[k] = mapAngleToVal(minMaxValues3[k].first, minMaxValues3[k].second, joints3[k] + offsetJoints3[k]);
+            tim3[k] = 500;
+            spee3[k] = 0;
+            cont2++;
+        }
+        
+    }
+    guard.lock(); 
+    servoDriver->SyncWritePos(id3, idn3, pos3, tim3, spee3);
+    guard.unlock();
+    auto t5 = std::chrono::high_resolution_clock::now();
+    float duration3 = std::chrono::duration_cast<std::chrono::milliseconds>(t5-t4).count();
+    std::cout << "Time send joints both ARMS: " << std::to_string(duration3) << std::endl;
+    std::cout << "Position sended both ARMS: " << std::endl;
+    for(unsigned k = 0; k < idn3; k++){
+        std::cout << "  " << std::to_string(id3[k]) << "  " << pos3[k] << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    //////////////////////////////////////////////////
+    // READ ARM1
+    auto t6 = std::chrono::high_resolution_clock::now();
+    std::vector<float> joints_read1;
+    for(unsigned l = 0; l < 4; l++){
+        guard.lock();
+        int val = servoDriver->ReadPos(10 + l + 1);
+        guard.unlock();
+        if(val < 0 || val > 1024){
+            joints_read1.push_back(-1);
+        }else{
+            joints_read1.push_back( mapValToAngle( minMaxValues1[l].first, 
+                                            minMaxValues1[l].second,
+                                            val) - offsetJoints1[l]);
+        }
+    }
+    auto t7 = std::chrono::high_resolution_clock::now();
+    float duration4 = std::chrono::duration_cast<std::chrono::milliseconds>(t7-t6).count();
+    std::cout << "Time read joints ARM1: " << std::to_string(duration4) << std::endl;
+    std::cout << "Read ARM1: " << std::endl;
+    for(unsigned l = 0; l < 4; l++){
+        std::cout << "  " << joints_read1[l] << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    // READ ARM2
+    auto t8 = std::chrono::high_resolution_clock::now();
+    std::vector<float> joints_read2;
+    for(unsigned m = 0; m < 4; m++){
+        guard.lock();
+        int val = servoDriver->ReadPos(20 + m + 1);
+        guard.unlock();
+        if(val < 0 || val > 1024){
+            joints_read2.push_back(-1);
+        }else{
+            joints_read2.push_back( mapValToAngle( minMaxValues2[m].first, 
+                                            minMaxValues2[m].second,
+                                            val) - offsetJoints2[m]);
+        }
+    }
+    auto t9 = std::chrono::high_resolution_clock::now();
+    float duration5 = std::chrono::duration_cast<std::chrono::milliseconds>(t9-t8).count();
+    std::cout << "Time read joints ARM2: " << std::to_string(duration5) << std::endl;
+    std::cout << "Read ARM2: " << std::endl;
+    for(unsigned m = 0; m < 4; m++){
+        std::cout << "  " << joints_read2[m] << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    // READ BOTH ARMS
+    auto t10 = std::chrono::high_resolution_clock::now();
+    std::vector<float> joints_read3;
+    int cont3 = 0, cont4 = 0;
+    for(unsigned n = 0; n < 8; n++){
+        if(n < 4){
+            guard.lock();
+            int val = servoDriver->ReadPos(10 + cont3 + 1);
+            guard.unlock();
+            if(val < 0 || val > 1024){
+                joints_read3.push_back(-1);
+            }else{
+                joints_read3.push_back( mapValToAngle( minMaxValues1[cont3].first, 
+                                                minMaxValues1[cont3].second,
+                                                val) - offsetJoints1[cont3]);
+            }
+            cont3++;
+        }else{
+            guard.lock();
+            int val = servoDriver->ReadPos(20 + cont4 + 1);
+            guard.unlock();
+            if(val < 0 || val > 1024){
+                joints_read3.push_back(-1);
+            }else{
+                joints_read3.push_back( mapValToAngle( minMaxValues2[cont4].first, 
+                                                minMaxValues2[cont4].second,
+                                                val) - offsetJoints2[cont4]);
+            }
+            cont4++;
+        }    
+    }
+    auto t11 = std::chrono::high_resolution_clock::now();
+    float duration6 = std::chrono::duration_cast<std::chrono::milliseconds>(t11-t10).count();
+    std::cout << "Time read joints both ARMS: " << std::to_string(duration6) << std::endl;
+    std::cout << "Read both ARMS: " << std::endl;
+    for(unsigned n = 0; n < 8; n++){
+        std::cout << "  " << joints_read3[n] << std::endl;
+    }
+    std::cout << std::endl;
 
     return 0;
 }
