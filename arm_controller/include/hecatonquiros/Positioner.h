39ea0944e58513b4ee39f6e6fb471d32ce5601c5
pabramsor <pabramsor@gmail.com>
2018-09-27 09:32:46 +0200

    Added positioner tester and namespace to Positioner class

--- arm_controller/include/hecatonquiros/Positioner.h
+++ arm_controller/include/hecatonquiros/Positioner.h
@@ -25,84 +25,81 @@
 #include <thread>
 #include <mutex>
 
-struct Potentiometer {
-	float valToAngle(float _val) {
-		return _val / 1023 * (mMaxAngle - mMinAngle) + mMinAngle;
-	}
+namespace hecatonquiros {
 
-	float mMinAngle;
-	float mMaxAngle;
-};
+	struct Potentiometer {
+		float valToAngle(float _val) {
+			return _val / 1023 * (mMaxAngle - mMinAngle) + mMinAngle;
+		}
 
-class Positioner{
-public:
-	/// Construct positioner hal given the port id and baudrate.
-	/// \param _port: string with path to serial port file.
-	/// \param _baudrate: baudrate for the serial port.
-	Positioner(std::string _port, int _baudrate);
+		float mMinAngle;
+		float mMaxAngle;
+	};
 
-	/// Construct positioner hal given a serial connection.
-	/// \param _serialPort: already connected serial port.
-	Positioner(serial::Serial *_serialPort);
+	class Positioner{
+	public:
+		/// Construct positioner hal given the port id and baudrate.
+		/// \param _port: string with path to serial port file.
+		/// \param _baudrate: baudrate for the serial port.
+		Positioner(std::string _port, int _baudrate);
 
-	/// Initialize all the internal members
-    void init();
+		/// Construct positioner hal given a serial connection.
+		/// \param _serialPort: already connected serial port.
+		Positioner(serial::Serial *_serialPort);
 
-	/// Deinitialize all internal members
-	/// \return: true or false if deinitialization of the internal member went good or bad
-	bool close();
+		/// Deinitialize all internal members
+		/// \return: true or false if deinitialization of the internal member went good or bad
+		bool close();
 
-	/// Returns the relative position of the hand from the base of the positioner
-	/// \param _x: 
-	/// \param _y: 
-	/// \param _z: 
-	void baseToHand (float &_x, float &_y, float &_z);
-	
-	/// Returns the relative position of the base from the handle of the positioner
-	/// \param _x: /// \param _t0:
-	/// \param _y: 
-	/// \param _z:
-	void handToBase (float &_x, float &_y, float &_z);
-	
-	/// Returns the raw values of the potentiomenters from 0 to 1023
-	/// \param _j0: 
-	/// \param _j1: 
-	/// \param _j2: 
-	/// \param _j3:
-	void rawJoints  (float &_j0, float &_j1, float &_j2, float &_j3, float &_j4);
-	
-	/// Returns the angles of the joints
-	/// \param _t0: 
-	/// \param _t1: 
-	/// \param _t2: 
-	/// \param _t3:
-	void angles     (float &_t0, float &_t1, float &_t2, float &_t3, float &_j4);
+		/// Returns the relative position of the hand from the base of the positioner
+		/// \param _x: 
+		/// \param _y: 
+		/// \param _z: 
+		void baseToHand (float &_x, float &_y, float &_z);
+		
+		/// Returns the relative position of the base from the handle of the positioner
+		/// \param _x: /// \param _t0:
+		/// \param _y: 
+		/// \param _z:
+		void handToBase (float &_x, float &_y, float &_z);
+		
+		/// Returns the raw values of the potentiomenters from 0 to 1023
+		/// \param _j0: 
+		/// \param _j1: 
+		/// \param _j2: 
+		/// \param _j3: 
+		/// \param _j4:
+		void rawJoints  (float &_j0, float &_j1, float &_j2, float &_j3, float &_j4);
+		
+		/// Returns the angles of the joints
+		/// \return angles of the potentiometer in a vector
+		std::vector<float> angles     ();
 
-    /// Returns the angles of the joints
-	/// \return angles of the potentiometer in a vector
-	std::vector<float> angles     ();
+		/// Returns the transforms of the joints
+		/// \param _T01:
+		/// \param _T12:
+		/// \param _T23:
+		/// \param _T34: 
+		void lastTransforms(Eigen::Matrix4f &_T01, Eigen::Matrix4f &_T12, Eigen::Matrix4f &_T23, Eigen::Matrix4f & _T34, Eigen::Matrix4f & _T4f);
 
-	/// Returns the transforms of the joints
-	/// \param _T01:
-	/// \param _T12:
-	/// \param _T23:
-	/// \param _T34: 
-	void lastTransforms(Eigen::Matrix4f &_T01, Eigen::Matrix4f &_T12, Eigen::Matrix4f &_T23, Eigen::Matrix4f & _T34, Eigen::Matrix4f & _T4f);
+	private:
+		/// Initialize all the internal members
+		void init();
 
-private:
-	serial::Serial	*mArduinoCom;
-	std::mutex		mSecureRead;
-	std::thread		mSerialThread;
-	bool			mRun = true;
+	private:
+		serial::Serial	*mArduinoCom;
+		std::mutex		mSecureRead;
+		std::thread		mSerialThread;
+		bool			mRun = true;
 
-	float mJ0, mJ1, mJ2, mJ3, mJ4;
-	Eigen::Matrix<float,4,4,Eigen::DontAlign> mT01, mT12, mT23, mT34, mT4f;
-	Potentiometer mP0, mP1, mP2, mP3, mP4;
+		float mJ0, mJ1, mJ2, mJ3, mJ4;
+		Eigen::Matrix<float,4,4,Eigen::DontAlign> mT01, mT12, mT23, mT34, mT4f;
+		Potentiometer mP0, mP1, mP2, mP3, mP4;
 
-	const float cL01 = 0.066f; //0.068f;
-	const float cL12 = 0.104f; //0.169f;
-	const float cL23 = 0.153f; //0.169f;
-    const float cL34 = 0.068f; //0.142f;
-    const float cL4f = 0.08f; //0.08f;
-};
-
+		const float cL01 = 0.066f; //0.068f;
+		const float cL12 = 0.104f; //0.169f;
+		const float cL23 = 0.153f; //0.169f;
+		const float cL34 = 0.068f; //0.142f;
+		const float cL4f = 0.08f; //0.08f;
+	};
+}
--- arm_controller/src/Positioner.cpp
+++ arm_controller/src/Positioner.cpp
@@ -23,140 +23,142 @@
 #include <hecatonquiros/Positioner.h>
 
 #include <iostream>
+namespace hecatonquiros{
+    
+    //---------------------------------------------------------------------------------------------------------------------
+    Positioner::Positioner(std::string _port, int _baudrate) {
+        mArduinoCom = new serial::Serial(_port, _baudrate, serial::Timeout::simpleTimeout(1000));
+        init();
+    }
 
-//---------------------------------------------------------------------------------------------------------------------
-Positioner::Positioner(std::string _port, int _baudrate) {
-    mArduinoCom = new serial::Serial(_port, _baudrate, serial::Timeout::simpleTimeout(1000));
-    init();
-}
+    //---------------------------------------------------------------------------------------------------------------------
+    Positioner::Positioner(serial::Serial *_serialPort) {
+        mArduinoCom = _serialPort;
+        init();
+    }
 
-//---------------------------------------------------------------------------------------------------------------------
-Positioner::Positioner(serial::Serial *_serialPort) {
-    mArduinoCom = _serialPort;
-    init();
-}
+    //---------------------------------------------------------------------------------------------------------------------
+    void Positioner::init() {
+        mP0 = {-230.0f/2,   230.0f/2}; //{-230.0f/2,   230.0f/2};
+        mP1 = {-130.0f,     125.0f }; //{-120.0f, 140.0f };
+        mP2 = {-110.0f,     115.0f }; //{-130.0f, 115.0f };
+        mP3 = { -115.0f,    115.0f }; //{ -130.0f, 135.0f };
+        mP4 = { -130.0f,    135.0f }; //{ -130.0f, 135.0f };
 
-//---------------------------------------------------------------------------------------------------------------------
-void Positioner::init() {
-    mP0 = {-230.0f/2,   230.0f/2}; //{-230.0f/2,   230.0f/2};
-    mP1 = {-130.0f,     125.0f }; //{-120.0f, 140.0f };
-    mP2 = {-110.0f,     115.0f }; //{-130.0f, 115.0f };
-    mP3 = { -115.0f,    115.0f }; //{ -130.0f, 135.0f };
-    mP4 = { -130.0f,    135.0f }; //{ -130.0f, 135.0f };
-
-    mSerialThread = std::thread([&]() {
-        std::string data = "";
-        while (mRun) {
-            mSecureRead.lock();
-            mArduinoCom->write("p\r\n");
-            std::string data = mArduinoCom->readline();
-            try {
-                if (data.find_first_of(",") != data.npos) {
-                    mJ4 = atof(data.substr(0, data.find_first_of(",")).c_str());
-                    data = data.substr(data.find_first_of(",") + 1, data.size());
-                    mJ3 = atof(data.substr(0, data.find_first_of(",")).c_str());
-                    data = data.substr(data.find_first_of(",") + 1, data.size());
-                    mJ2 = atof(data.substr(0, data.find_first_of(",")).c_str());
-                    data = data.substr(data.find_first_of(",") + 1, data.size());
-                    mJ1 = atof(data.substr(0, data.find_first_of(",")).c_str());
-                    data = data.substr(data.find_first_of(",") + 1, data.size());
-                    mJ0 = atof(data.c_str());
+        mSerialThread = std::thread([&]() {
+            std::string data = "";
+            while (mRun) {
+                mSecureRead.lock();
+                mArduinoCom->write("p\r\n");
+                std::string data = mArduinoCom->readline();
+                try {
+                    if (data.find_first_of(",") != data.npos) {
+                        mJ4 = atof(data.substr(0, data.find_first_of(",")).c_str());
+                        data = data.substr(data.find_first_of(",") + 1, data.size());
+                        mJ3 = atof(data.substr(0, data.find_first_of(",")).c_str());
+                        data = data.substr(data.find_first_of(",") + 1, data.size());
+                        mJ2 = atof(data.substr(0, data.find_first_of(",")).c_str());
+                        data = data.substr(data.find_first_of(",") + 1, data.size());
+                        mJ1 = atof(data.substr(0, data.find_first_of(",")).c_str());
+                        data = data.substr(data.find_first_of(",") + 1, data.size());
+                        mJ0 = atof(data.c_str());
+                    }
                 }
+                catch (int _e){
+                    std::cout << "Read exception" << std::endl;
+                }
+                mSecureRead.unlock();
+                //std::cout << mJ0 << ", " << mJ1 << ", " << mJ2 << ", " << mJ3 << ", " << mJ4 << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
             }
-            catch (int _e){
-                std::cout << "Read exception" << std::endl;
-            }
-            mSecureRead.unlock();
-            //std::cout << mJ0 << ", " << mJ1 << ", " << mJ2 << ", " << mJ3 << ", " << mJ4 << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        }
-    });
+        });
 
-    std::cout << "Initialized Positioner" << std::endl;
-}
+        std::cout << "Initialized Positioner" << std::endl;
+    }
 
-//---------------------------------------------------------------------------------------------------------------------
-bool Positioner::close() {
-	mRun = false;
-	while (!mSerialThread.joinable()) {}
-	mSerialThread.join();
+    //---------------------------------------------------------------------------------------------------------------------
+    bool Positioner::close() {
+        mRun = false;
+        while (!mSerialThread.joinable()) {}
+        mSerialThread.join();
 
-	return true;
-}
+        return true;
+    }
 
-//---------------------------------------------------------------------------------------------------------------------
-void Positioner::baseToHand(float &_x, float &_y, float &_z){
-    std::vector<float> vals = angles();
- 
+    //---------------------------------------------------------------------------------------------------------------------
+    void Positioner::baseToHand(float &_x, float &_y, float &_z){
+        std::vector<float> vals = angles();
+    
 
-    mT01 <<	cos(vals[0]), 0, sin(vals[0]), 0,
-            sin(vals[0]), 0, -cos(vals[0]), 0,
-			0, 1, 0, cL01,
-			0, 0, 0, 1;
-   
-    mT12 <<	cos(M_PI / 2 + vals[1]), -sin(M_PI / 2 + vals[1]), 0, (cL12 * cos(M_PI / 2 + vals[1])),
-            sin(M_PI / 2 + vals[1]), cos(M_PI / 2 + vals[1]), 0, (cL12 * sin(M_PI / 2 + vals[1])),
-			0, 0, 1, 0,
-			0, 0, 0, 1;
+        mT01 <<	cos(vals[0]), 0, sin(vals[0]), 0,
+                sin(vals[0]), 0, -cos(vals[0]), 0,
+                0, 1, 0, cL01,
+                0, 0, 0, 1;
+    
+        mT12 <<	cos(M_PI / 2 + vals[1]), -sin(M_PI / 2 + vals[1]), 0, (cL12 * cos(M_PI / 2 + vals[1])),
+                sin(M_PI / 2 + vals[1]), cos(M_PI / 2 + vals[1]), 0, (cL12 * sin(M_PI / 2 + vals[1])),
+                0, 0, 1, 0,
+                0, 0, 0, 1;
 
-    mT23<<	cos(vals[2]), -sin(vals[2]), 0, (cL23 * cos(vals[2])),
-            sin(vals[2]), cos(vals[2]), 0, (cL23 * sin(vals[2])),
-			0, 0, 1, 0,
-			0, 0, 0, 1;
+        mT23<<	cos(vals[2]), -sin(vals[2]), 0, (cL23 * cos(vals[2])),
+                sin(vals[2]), cos(vals[2]), 0, (cL23 * sin(vals[2])),
+                0, 0, 1, 0,
+                0, 0, 0, 1;
 
-  
-    mT34 <<	cos(vals[3]), 0, sin(vals[3]), (cL34 * cos(vals[3])),
-            sin(vals[3]), 0, -cos(vals[3]), (cL34 * sin(vals[3])),
-			0, 1, 0, 0,
-			0, 0, 0, 1;
+    
+        mT34 <<	cos(vals[3]), 0, sin(vals[3]), (cL34 * cos(vals[3])),
+                sin(vals[3]), 0, -cos(vals[3]), (cL34 * sin(vals[3])),
+                0, 1, 0, 0,
+                0, 0, 0, 1;
 
-    mT4f << cos(vals[4]), -sin(vals[4]), 0, (cL4f * cos(vals[4])),
-            sin(vals[4]), cos(vals[4]), 0, (cL4f * sin(vals[4])),
-			0, 0, 1, 0,
-			0, 0, 0, 1;
+        mT4f << cos(vals[4]), -sin(vals[4]), 0, (cL4f * cos(vals[4])),
+                sin(vals[4]), cos(vals[4]), 0, (cL4f * sin(vals[4])),
+                0, 0, 1, 0,
+                0, 0, 0, 1;
 
-	Eigen::Matrix4f finalT = mT01*mT12*mT23*mT34*mT4f;
+        Eigen::Matrix4f finalT = mT01*mT12*mT23*mT34*mT4f;
 
-	_x = finalT(0,3);
-	_y = finalT(1,3);
-	_z = finalT(2,3);
-  
-}
+        _x = finalT(0,3);
+        _y = finalT(1,3);
+        _z = finalT(2,3);
+    
+    }
 
-//---------------------------------------------------------------------------------------------------------------------
-void Positioner::handToBase(float &_x, float &_y, float &_z){
-  
-}
+    //---------------------------------------------------------------------------------------------------------------------
+    void Positioner::handToBase(float &_x, float &_y, float &_z){
+    
+    }
 
-//---------------------------------------------------------------------------------------------------------------------
-void Positioner::rawJoints(float &_j0, float &_j1, float &_j2, float &_j3, float &_j4){
-	mSecureRead.lock();
-	_j0 = mJ0;
-	_j1 = mJ1;
-	_j2 = mJ2;
-	_j3 = mJ3;
-	_j4 = mJ4;
-	mSecureRead.unlock();
-}
+    //---------------------------------------------------------------------------------------------------------------------
+    void Positioner::rawJoints(float &_j0, float &_j1, float &_j2, float &_j3, float &_j4){
+        mSecureRead.lock();
+        _j0 = mJ0;
+        _j1 = mJ1;
+        _j2 = mJ2;
+        _j3 = mJ3;
+        _j4 = mJ4;
+        mSecureRead.unlock();
+    }
 
-//---------------------------------------------------------------------------------------------------------------------
-std::vector<float> Positioner::angles(){
-    std::vector<float> vals(5);
-    mSecureRead.lock();
-    vals[0] = mP0.valToAngle(mJ0) / 180.0f*M_PI;//((mJ0 - 512.0f) / 512.0f) * (135.0f / 180.0f * M_PI);
-    vals[1] = mP1.valToAngle(mJ1) / 180.0f*M_PI;//((mJ1 - 512.0f) / 512.0f) * (135.0f / 180.0f * M_PI);
-    vals[2] = mP2.valToAngle(mJ2) / 180.0f*M_PI;//((mJ2 - 512.0f) / 512.0f) * (135.0f / 180.0f * M_PI);
-    vals[3] = mP3.valToAngle(mJ3) / 180.0f*M_PI;//((mJ3 - 512.0f) / 512.0f) * (135.0f / 180.0f * M_PI);
-    vals[4] = -mP4.valToAngle(mJ4) / 180.0f*M_PI;//((mJ3 - 512.0f) / 512.0f) * (135.0f / 180.0f * M_PI);
-	mSecureRead.unlock();
-    return vals;
-}
+    //---------------------------------------------------------------------------------------------------------------------
+    std::vector<float> Positioner::angles(){
+        std::vector<float> vals(5);
+        mSecureRead.lock();
+        vals[0] = mP0.valToAngle(mJ0) / 180.0f*M_PI;//((mJ0 - 512.0f) / 512.0f) * (135.0f / 180.0f * M_PI);
+        vals[1] = mP1.valToAngle(mJ1) / 180.0f*M_PI;//((mJ1 - 512.0f) / 512.0f) * (135.0f / 180.0f * M_PI);
+        vals[2] = mP2.valToAngle(mJ2) / 180.0f*M_PI;//((mJ2 - 512.0f) / 512.0f) * (135.0f / 180.0f * M_PI);
+        vals[3] = mP3.valToAngle(mJ3) / 180.0f*M_PI;//((mJ3 - 512.0f) / 512.0f) * (135.0f / 180.0f * M_PI);
+        vals[4] = -mP4.valToAngle(mJ4) / 180.0f*M_PI;//((mJ3 - 512.0f) / 512.0f) * (135.0f / 180.0f * M_PI);
+        mSecureRead.unlock();
+        return vals;
+    }
 
-//---------------------------------------------------------------------------------------------------------------------
-void Positioner::lastTransforms(Eigen::Matrix4f & _T01, Eigen::Matrix4f & _T12, Eigen::Matrix4f & _T23, Eigen::Matrix4f & _T34, Eigen::Matrix4f & _T4f) {
-	_T01 = mT01;
-	_T12 = mT12;
-	_T23 = mT23;
-	_T34 = mT34;
-	_T4f = mT4f;
+    //---------------------------------------------------------------------------------------------------------------------
+    void Positioner::lastTransforms(Eigen::Matrix4f & _T01, Eigen::Matrix4f & _T12, Eigen::Matrix4f & _T23, Eigen::Matrix4f & _T34, Eigen::Matrix4f & _T4f) {
+        _T01 = mT01;
+        _T12 = mT12;
+        _T23 = mT23;
+        _T34 = mT34;
+        _T4f = mT4f;
+    }
 }
