d6cb47dc93e68626d3b837189806eec00eb513bc
19Bardo91 <19Bardo91@gmail.com>
2019-07-22 15:45:49 +0200

    Fixed installation of python deps

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -55,7 +55,8 @@ install_git_repo "OpenSceneGraph" "https://github.com/openscenegraph/OpenSceneGr
 ###################################################################
 sudo apt-get install cmake g++ git ipython minizip python-dev python-h5py python-numpy python-scipy python-sympy qt4-dev-tools libassimp-dev libavcodec-dev libavformat-dev libavformat-dev libboost-all-dev libboost-date-time-dev libbullet-dev libfaac-dev libglew-dev libgsm1-dev liblapack-dev liblog4cxx-dev libmpfr-dev libode-dev libogg-dev libpcrecpp0v5 libpcre3-dev libqhull-dev libqt4-dev libsoqt-dev-common libsoqt4-dev libswscale-dev libswscale-dev libvorbis-dev libx264-dev libxml2-dev libxvidcore-dev libcairo2-dev libjasper-dev libpoppler-glib-dev libsdl2-dev libtiff5-dev libxrandr-dev libccd-dev
 
-pip install sympy==0.7.1.0 numpy numpy-quaternion numba
+pip install --user sympy==0.7.1.0 
+pip install --user numpy numpy-quaternion numba
 
 git clone https://github.com/rdiankov/openrave.git
 cd openrave
